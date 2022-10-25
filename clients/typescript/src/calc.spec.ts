b549e7890abddb08080aabf88542777bc4219c95
sergii.volodko <svolodko@8thlight.com>
2022-10-25 15:16:38 +0100

    make calculate single item in order

--- clients/typescript/src/calc.spec.ts
+++ clients/typescript/src/calc.spec.ts
@@ -1,4 +1,4 @@
-import { sayHello } from "./calc";
+import { sayHello, calculatePrice } from "./calc";
 
 
 describe("Test calc", () => {
@@ -11,4 +11,21 @@ describe("Test calc", () => {
         expect(sayHello()).toBe("Hello World");
     })
 
+
+    it('works for one item order', () => {
+
+        const order = {"prices":[15.99],"quantities":[1],"country":"ES","reduction":"STANDARD"};
+
+        const price = calculatePrice(order);
+        const expected = { total : 19.03};
+        expect(price).toBe(expected);
+    })
+    // it('works for one item order', () => {
+    //
+    //     const order = {"prices":[4.1,8.03,86.83,65.62,44.82],"quantities":[10,3,5,4,5],"country":"AT","reduction":"STANDARD"};
+    //
+    //     const price = calculatePrice(order);
+    //     const expected = {"total":1166.62};
+    //     expect(price).toBe(expected);
+    // })
 })
--- clients/typescript/src/calc.ts
+++ clients/typescript/src/calc.ts
@@ -1,5 +1,21 @@
 // more of your code here
 
+import { calcTax } from './taxCalculator';
+
 export function sayHello() {
     return "Hello World";
+}
+
+export function calculatePrice(order: any): { total: number } {
+
+    const itemPrice = order["prices"][0];
+    const quantity = order["quantities"][0];
+
+    let price = itemPrice * quantity;
+
+    const country = order["country"];
+
+    price = calcTax(price, country)
+
+    return { total: Number(price.toFixed(2)) };
 }
